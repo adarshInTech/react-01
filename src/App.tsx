451a12808dfd3640f1ee2a7a577e6887c93fb6b0
adarshInTech <aadarshbajpaii@gmail.com>
2024-08-11 15:49:15 +0530

    react basic

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import ListGroup from "./components/ListGroup";
+function App() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
